272ce432879a85ddc818e329e6a910c0e1207cad
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-18 18:19:37 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
 
 /**
@@ -13,17 +12,17 @@ const users = [
 ];
 
 // comment this out after completion and uncomment code below it to proceed
-function Child() {
-  return <div>This is children content</div>;
-}
-/**
-  Challenge: Uncomment this code to complete quiz
+// function Child() {
+//   return <div>This is children content</div>;
+// }
 
-function Child() {
+//  Challenge: Uncomment this code to complete quiz
+
+function Child({changeParent}) {
   return (
     <>
       <div>Child</div>
-      <button>Change Parent Value</button>
+      <button onClick={changeParent}>Change Parent Value</button>
     </>
   );
 }
@@ -32,9 +31,12 @@ function Parent() {
   const [value, setValue] = React.useState(
     "I need to be updated from my child"
   );
+  function valueChanged(){
+    setValue("Updated from child")
+  }
 
   return (
-    <>
+    <div className="value">
       <h3>Update Parent State Challenge (Using Callback)</h3>
       <div className="wrapper">
         <div>Parent</div>
@@ -42,40 +44,49 @@ function Parent() {
       </div>
 
       <div className="wrapper">
-        <Child />
+        <Child changeParent={valueChanged}/>
       </div>
-    </>
-  );
-}
-Uncomment this to tackle quiz
-**/
-
-// Comment out after completion
-function Parent() {
-  return (
-    <div>
-      <h3>Parent Component</h3>
     </div>
   );
 }
+// Uncomment this to tackle quiz
+
+
+// Comment out after completion
+// function Parent() {
+//   return (
+//     <div>
+//       <h3>Parent Component</h3>
+//     </div>
+//   );
+// }
 // Comment above code after completion
 
 function App() {
+
   const [] = React.useState(true);
+
+  const allUsers = users.map((allUsers) => <li key={allUsers.id}>{allUsers.name}</li>);
+
   return (
     
-    <>
+    <div className="names">
     
-    <img src={logo} className="App-logo" alt="logo" />
+    <img src="https://data.whicdn.com/images/190954249/original.gif" 
+    className="App-logo" 
+    alt="logo" />
+     <p>JSX is cool</p>
       <h3>User names</h3>
-      <ul></ul>
+      <ul>
+        {allUsers}
+      </ul>
       <button>Hide Element Below</button>
 
       <div>Toggle Challenge</div>
       <Parent>
       <Child />
     </Parent>
-    </>
+    </div>
   );
 }
 
